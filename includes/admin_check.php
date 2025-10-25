c08ef79082cd923342729be7e10ebefc17fb61d2
Salama-Shali <roselyne.mwalagho@strathmore.edu>
2025-10-25 17:47:51 +0300

    admin check

---
+++ includes/admin_check.php
@@ -0,0 +1,17 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../../modules/auth/login.php");
+    exit;
+}
+
+// check if the user is an admin
+if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
+    header("Location: ../../index.php");
+    exit;
+}
+?>
